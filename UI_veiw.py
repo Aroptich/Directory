8842561c12f55c060e503c6e95b4c47ef8c4104a
Alexsandr <artamoshkinas@gmail.com>
2022-12-21 15:30:14 +0700

    work out the display of 2 windows

--- UI_veiw.py
+++ UI_veiw.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
+import sys
 
 
 class Widget(QWidget):
@@ -14,7 +15,7 @@ class Widget(QWidget):
         self.btn2 = QPushButton('Создать контакт', self)  # Создаем кнопку на главном окне
         self.btn2.setFont(QFont("Times", 11, QFont.Bold))  # Задание шрифта виджета
         self.btn2.setGeometry(QRect(230, 330, 150, 50))  # Задание положения и размеров поля виджета
-    def creat_label(self):
+    def create_label(self):
         self.label_names = ['Имя', 'Фамилия', 'Номер телефона']
         for names in self.label_names:
             self.label = QLabel(names, self)
@@ -24,10 +25,16 @@ class Widget(QWidget):
         return self.label
 
 
+    def create_menu(self):
+        menuBar = self.menuBar()
+        fileMenu = QMenu("Файл", self)
+        menuBar.addMenu(fileMenu)
+        list_contacts = fileMenu.addAction('Список контактов')
+        # list_contacts.triggered.connect(lambda: Guitar_2())
+        return menuBar
 
 
-
-    def creat_line_edit(self):
+    def create_line_edit(self):
         self.y = 150
         self.line_edit_names = ['Введите Имя', 'Введите фамилию', 'Введите номер телефона']
         for names in self.line_edit_names:
@@ -37,4 +44,24 @@ class Widget(QWidget):
             self.ledText.setGeometry(QRect(self.x, self.y, self.widht_widget, self.height_widget)) #Задание положения и размеров поля виджета
             self.y +=50
         return self.ledText
+
+# class Guitar_2(QWidget):
+#     def __init__(self):
+#         super().__init__()
+#
+#         self.setGeometry(250, 55, 1200, 800)
+#         self.setWindowTitle('Гитара')
+#
+#         self.First_button = QPushButton('Первая струна(клавиша 1)', self)
+#         self.First_button.resize(170, 50)
+#         self.First_button.move(40, 100)
+#
+#         self.Second_button = QPushButton('Вторая струна(клавиша 2)', self)
+#         self.Second_button.resize(170, 50)
+#         self.Second_button.move(40, 200)
+#         self.show()
+
+
+
+
 
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import sys
 from PyQt5.QtWidgets import *
 from PyQt5 import QtGui
-from UI_veiw import Widget
+from UI_veiw import Widget, Guitar_2
 from PyQt5.QtCore import *
+from PyQt5.QtWidgets import QMenuBar
 
 
 
-class DlgMain(QMainWindow, Widget): #Создаем класс DlgMain и наследуемся от класса Qdialog
+class DlgMain(QMainWindow, Widget, QMenuBar): #Создаем класс DlgMain и наследуемся от класса Qdialog
     def __init__(self): # Функция инициализации класса
         super().__init__() #Вызываем конструктор класса родителя(QMainWindow)
         self.setWindowTitle('Справочник') #Назввние приложения
@@ -16,9 +17,12 @@ class DlgMain(QMainWindow, Widget): #Создаем класс DlgMain и нас
 
 
         self.setStyleSheet('.DlgMain {background-image: url(m_directory-yellow.jpg);}') # Устанавливаем задний фон
-        self.label = Widget.creat_label(self) # Создаем именованные лейблы на главном окне
-        self.line_edit = Widget.creat_line_edit(self)  # Создаем именнованные поля ввода на главном окне
+        self.label = Widget.create_label(self) # Создаем именованные лейблы на главном окне
+        self.line_edit = Widget.create_line_edit(self)  # Создаем именнованные поля ввода на главном окне
         self.btn = Widget() #Создаем кнопку на главном экране
+        self.menu = Widget.create_menu(self) # Создаем меню с встпылвающими подменю
+        # self.win = Guitar_2()
+
 
         self.statusbar = self.statusBar()  # Создаем статус в нижнем углу экрана на главном окне для отображения сообщений
         self.statusbar.showMessage('Готово', 1000)  # Отображение сообщения статус бара и продолжительность в мс
